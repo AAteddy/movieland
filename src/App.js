3901e5f7e13394b1521f2d98383f61fc7e1de298
Tedros Alem <tedsaasfaha@gmail.com>
2023-08-01 12:35:21 +0300

    corrected map() error and finilize the app

--- src/App.js
+++ src/App.js
@@ -6,50 +6,55 @@ import MovieCard from './MovieCard';
 const API_URL = 'http://www.omdbapi.com?apikey=45faacc6';
 
 const App = () => {
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState([]);
 
   const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`)
+    const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-
     setMovies(data.Search);
-  }
+  };
+
   useEffect(() => {
     searchMovies('John Wick');
-  }, []);
+  }, [searchTerm]);
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+        searchMovies(searchTerm);
+    }
+  };
 
   return (
     <div className="app">
       <h1>MovieLand</h1>
       <div className='search'>
         <input
-          value="John Wick"
+          value={searchTerm}
           placeholder="Search for Movies"
-          onChange={() => {}}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={handleKeyPress}
           />
           <img
             src={SearchIcon}
             alt='search'
-            onClick={() => {}}
+            onClick={() => {searchMovies(searchTerm)}}
           />         
       </div>
-      {
-        movies?.length > 0
+      { movies?.length > 0
         ? (
           <div className="container">
-            {
-              movies.map((movie) => (
-                <MovieCard movie={movie}/>
-              ))}                
+            { movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}                
           </div>
-        ) 
-        : (
-          <div>
+        ) : (
+          <div className="empty">
             <h2>No Movies Found</h2>
           </div>
         )}     
     </div>
   );
-}
+};
 
 export default App;
